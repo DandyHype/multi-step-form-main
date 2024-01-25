e393529f5e8ffcb94696527c1ac24d9c2e25b9b3
Dumitru Bragari <dumitru@bragari.es>
2024-01-25 17:53:10 +0100

    functional prototype

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
     var step = 0;
 
@@ -28,13 +28,16 @@ document.addEventListener("DOMContentLoaded", function(){
         switch: document.querySelector(".subscription__swithcer")
     }
 
-    var[arcade, advanced, pro] = document.querySelectorAll(".card__price")
+    var [arcade, advanced, pro, onlineService, extraStorage, customProfile] = document.querySelectorAll(".card__price, .card__price-addon")
     const planPrices = {
         arcade: arcade,
         advanced: advanced,
-        pro: pro
+        pro: pro,
+        onlineService: onlineService,
+        extraStorage: extraStorage,
+        customProfile: customProfile
     }
-    
+
 
     const subPlan = {
         monthly: document.querySelector(".subscription__monthly"),
@@ -44,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const discountMsg = document.querySelectorAll(".card__discount");
 
     const prices = {
-         arcade: {
+        arcade: {
             monthly: 9,
             yearly: 90
         },
@@ -55,23 +58,37 @@ document.addEventListener("DOMContentLoaded", function(){
         pro: {
             monthly: 15,
             yearly: 150
+        },
+        onlineService: {
+            monthly: 1,
+            yearly: 10
+        },
+        extraStorage: {
+            monthly: 2,
+            yearly: 20
+        },
+        customProfile: {
+            monthly: 2,
+            yearly: 20
         }
+
+
 
     }
 
     var userChoice = {
 
-        yearlyPlan : false,
+        yearlyPlan: false,
 
         userPlan: {
-            arcade : true,
-            advanced : false,
-            pro : false
+            arcade: true,
+            advanced: false,
+            pro: false
         },
         userExtras: {
-            onlineService : false,
-            extraStorage : false,
-            customProfile : false
+            onlineService: false,
+            extraStorage: false,
+            customProfile: false
         }
     }
 
@@ -83,9 +100,9 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const content = document.querySelectorAll(".main__content");
 
-    const resumeAddons = document.querySelector(".resume__addons");
-    
-    
+
+
+
 
     function showError(credential) {
         credential.msg.classList.remove("hidden");
@@ -101,19 +118,19 @@ document.addEventListener("DOMContentLoaded", function(){
         resumeAddons.document.write('<div class="addons__service">');
     }
 
-    function checkCredentials(name, email, phone){
+    function checkCredentials(name, email, phone) {
         let flag = true;
-        if(name.input.value == ""){
+        if (name.input.value == "") {
             showError(name);
             flag = false;
-        }else 
+        } else
             hideError(name);
-        if(email.input.value == ""){
+        if (email.input.value == "") {
             showError(email);
             flag = false;
-        }else 
+        } else
             hideError(email);
-        if(phone.input.value == ""){
+        if (phone.input.value == "") {
             showError(phone);
             flag = false;
         } else
@@ -122,44 +139,44 @@ document.addEventListener("DOMContentLoaded", function(){
         return flag;
     }
 
-    function switchPlanButton(button){
-        if(button.style.justifyContent === "") {
+    function switchPlanButton(button) {
+        if (button.style.justifyContent === "") {
             button.style.justifyContent = "flex-end";
             userChoice.yearlyPlan = true;
             discountMsg.forEach(message => {
                 message.style.display = "block";
             });
-            for(var plan in planPrices){
-                planPrices[plan].innerHTML = "$" + prices[plan].yearly + "/yr";
+            for (var plan in planPrices) {
+                planPrices[plan].innerHTML = (String(plan).length > 9 ? "+$" : "$") + prices[plan].yearly + "/yr";
             }
             subPlan.monthly.style.color = "var(--clr-neutral-500)";
             subPlan.yearly.style.color = "var(--clr-primary)";
-        }else if(button.style.justifyContent === "flex-end") {
+        } else if (button.style.justifyContent === "flex-end") {
             button.style.justifyContent = "";
             userChoice.yearlyPlan = false;
             discountMsg.forEach(message => {
                 message.style.display = "none";
             });
-            for(var plan in planPrices){
-                planPrices[plan].innerHTML = "$" + prices[plan].monthly + "/mo";
+            for (var plan in planPrices) {
+                planPrices[plan].innerHTML = (String(plan).length > 9 ? "+$" : "$") + prices[plan].monthly + "/mo";
             }
             subPlan.monthly.style.color = "var(--clr-primary)";
             subPlan.yearly.style.color = "var(--clr-neutral-500)";
         }
     }
 
-    buttons.next.onclick = function() {
-        switch(step){
+    buttons.next.onclick = function () {
+        switch (step) {
             case 0:
-                if(checkCredentials(name, email, phone)) {
-                navSteps[step].classList.remove("link__number-active");
-                navSteps[step + 1].classList.add("link__number-active");
-                content[step].style.display = "none";
-                content[step + 1].style.display = "grid";
-                step++;
+                if (checkCredentials(name, email, phone)) {
+                    navSteps[step].classList.remove("link__number-active");
+                    navSteps[step + 1].classList.add("link__number-active");
+                    content[step].style.display = "none";
+                    content[step + 1].style.display = "grid";
+                    step++;
                 }
-                
-            break;
+
+                break;
 
             case 1:
                 navSteps[step].classList.remove("link__number-active");
@@ -167,7 +184,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 content[step].style.display = "none";
                 content[step + 1].style.display = "grid";
                 step++;
-            break;
+                break;
 
             case 2:
                 navSteps[step].classList.remove("link__number-active");
@@ -176,27 +193,28 @@ document.addEventListener("DOMContentLoaded", function(){
                 buttons.finish.style.display = "block";
                 content[step].style.display = "none";
                 content[step + 1].style.display = "grid";
+                createSummary()
                 step++;
-            break;
+                break;
 
             case 3:
-                
+
                 content[step].style.display = "none";
                 content[step + 1].style.display = "grid";
                 step++;
-            break;
+                break;
 
             case 4:
                 content[step].style.display = "none";
                 content[step + 1].style.display = "grid";
-            break;
+                break;
 
         }
 
-        
+
     }
 
-    buttons.back.onclick = function() {
+    buttons.back.onclick = function () {
         switch (step) {
             case 4:
                 navSteps[step].classList.remove("link__number-active");
@@ -205,7 +223,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 content[step - 1].style.display = "grid";
                 step--;
                 break;
-        
+
             case 3:
                 navSteps[step].classList.remove("link__number-active");
                 navSteps[step - 1].classList.add("link__number-active");
@@ -214,15 +232,15 @@ document.addEventListener("DOMContentLoaded", function(){
                 content[step].style.display = "none";
                 content[step - 1].style.display = "grid";
                 step--;
-            break;
-            
+                break;
+
             case 2:
                 navSteps[step].classList.remove("link__number-active");
                 navSteps[step - 1].classList.add("link__number-active");
                 content[step].style.display = "none";
                 content[step - 1].style.display = "grid";
                 step--;
-            break;
+                break;
 
             case 1:
                 navSteps[step].classList.remove("link__number-active");
@@ -231,30 +249,78 @@ document.addEventListener("DOMContentLoaded", function(){
                 content[step - 1].style.display = "grid";
                 step--;
 
-            break;
+                break;
 
             case 0:
-            break;
+                break;
 
         }
     }
 
-    buttons.finish.onclick = function(){
+    buttons.finish.onclick = function () {
         content[step].style.display = "none";
         content[step + 1].style.display = "grid";
         buttons.navButtons.style.display = "none";
     }
 
-    buttons.switch.addEventListener("click", function(){
+    function createSummary() {
+        let titleH2 = document.querySelector(".selection__title");
+        let priamryPriceSelector = document.querySelector(".primary__price");
+        let addonsService = document.querySelectorAll(".addons__service");
+        let total = 0;
+        let totalTitle = document.querySelector(".total__title");
+        let totalPrice = document.querySelector(".total__price");
+
+        titleH2.innerHTML = Object.keys(userChoice.userPlan)
+            .map(choice => {
+                if (userChoice.userPlan[choice] == true) {
+                    userChoice.yearlyPlan ?
+                        priamryPriceSelector.innerHTML = '$' + prices[choice].yearly + '/yr' :
+                        priamryPriceSelector.innerHTML = '$' + prices[choice].monthly + '/mo';
+                    return String(choice);
+                }
+            })
+            .join('');
+        userChoice.yearlyPlan ? titleH2.innerHTML += '(Yearly)' : titleH2.innerHTML += '(Monthly)';
+
+        addonsService.forEach(function (service) {
+            if (userChoice.userExtras[service.dataset.extras]) {
+                service.style.display = "flex";
+                userChoice.yearlyPlan ?
+                    service.querySelector(".service__price").innerHTML = '+$' + prices[service.dataset.extras].yearly + '/yr' :
+                    service.querySelector(".service__price").innerHTML = '+$' + prices[service.dataset.extras].monthly + '/mo';
+            } else {
+                service.style.display = "none";
+            }
+        })
+
+        Object.keys(userChoice.userPlan).forEach( function(plan) {
+            userChoice.userPlan[plan] ? total += prices[plan].monthly : total;
+        });
+
+        Object.keys(userChoice.userExtras).forEach( function(extra) {
+            userChoice.userExtras[extra] ? total += prices[extra].monthly : total;
+            
+        });
+
+        userChoice.yearlyPlan ? total *= 10 : total;
+        userChoice.yearlyPlan ? totalTitle.innerHTML = 'Total (per year)' : totalTitle.innerHTML = 'Total (per month)';
+        userChoice.yearlyPlan ? totalPrice.innerHTML = '$' + total + "/yr": totalPrice.innerHTML = '$' + total + "/mo";
+        
+
+
+    }
+
+    buttons.switch.addEventListener("click", function () {
         switchPlanButton(buttons.switch);
     });
 
-    planCards.forEach(function(card) {
-        card.addEventListener("click", function() {
+    planCards.forEach(function (card) {
+        card.addEventListener("click", function () {
             var self = this;
-            planCards.forEach(function(card) {
+            planCards.forEach(function (card) {
                 card.classList.remove("card-selected");
-                
+
                 userChoice.userPlan[card.dataset.plan] = card.dataset.plan == self.dataset.plan;
             });
             this.classList.add("card-selected");
@@ -263,14 +329,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
     });
 
-    extrasCards.forEach(function(card) {
-        card.addEventListener("click", function() {
+    extrasCards.forEach(function (card) {
+        card.addEventListener("click", function () {
 
-            if(this.classList.contains('card-selected')){
+            if (this.classList.contains('card-selected')) {
                 this.classList.remove("card-selected");
                 this.querySelector('input[type="checkbox"]').checked = false;
                 userChoice.userExtras[this.dataset.extras] = false;
-            } else if(this.classList.contains('card-selected') == false) {
+            } else if (this.classList.contains('card-selected') == false) {
                 this.classList.add("card-selected");
                 this.querySelector('input[type="checkbox"]').checked = true;
                 userChoice.userExtras[this.dataset.extras] = true;
@@ -280,6 +346,6 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
 
-    
-    
+
+
 });
